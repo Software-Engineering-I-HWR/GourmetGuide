6e949f727c368bc72290d355c0e8cc2c5334bb8c
Canoob <carlo.gliech@gmx.de>
2024-10-05 06:38:28 +0200

    update node_package

--- Frontend/node_modules/@typescript-eslint/parser/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { parse, parseForESLint, ParserOptions } from './parser';
-export { clearCaches, createProgram, ParserServices, ParserServicesWithoutTypeInformation, ParserServicesWithTypeInformation, withoutProjectParserOptions, } from '@typescript-eslint/typescript-estree';
-export declare const version: string;
-export declare const meta: {
-    name: string;
-    version: string;
-};
-//# sourceMappingURL=index.d.ts.map
