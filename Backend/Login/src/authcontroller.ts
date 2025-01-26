e2e24bddad32fb79441d9871aff80e00f8e6f54f
Canoob <carlo.gliech@gmx.de>
2025-01-26 17:57:36 +0100

    add admin check

--- Backend/Login/src/authcontroller.ts
+++ Backend/Login/src/authcontroller.ts
@@ -1,12 +1,13 @@
 import {Request, Response} from "express";
 import {addUser, findUserByEmail} from "./database";
 import jwt from 'jsonwebtoken';
+import {jwtDecode} from "jwt-decode";
+import {jwtsecret} from "../../../config/jwt-secret.json";
+import {unlocked_users} from '../../../config/unlocked-users.json';
 
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const {jwtsecret} = require('../../../config/jwt-secret.json');
-
 
 app.use(cors());
 
@@ -41,4 +42,25 @@ export const register = async (req: Request, res: Response) => {
     const success = await addUser(email, password);
 
     return res.status(success).json({message: "Registrierung erfolgreich"});
+};
+
+export const adminCheck = async (req: Request, res: Response) => {
+    const {token} = req.body;
+
+    try {
+        const decoded: any = jwtDecode(token);
+
+        const isAuthorized = unlocked_users.some(
+            (user) => user.username === decoded.email
+        );
+
+        if (isAuthorized) {
+            return res.status(200).json({ message: 'Authorized' });
+        } else {
+            return res.status(403).json({ message: 'Not authorized' });
+        }
+    } catch (error) {
+        console.error('Fehler beim Decodieren des Tokens:', error);
+        return res.status(400).json({ message: 'Invalid token' });
+    }
 };
