bb2ce54ad6af915ee6b58f847dcdc9b7ce89f40d
blnpurple <turbo.bine@4-birkners.de>
2025-02-01 13:12:03 +0100

    improve start page

--- Frontend/src/components/Home/Home.tsx
+++ Frontend/src/components/Home/Home.tsx
@@ -127,7 +127,7 @@ const Home: React.FC = () => {
             <main className="main-content">
                 <section className="recipes">
                     <div className="row justify-content-center align-items-center">
-                         <button
+                        <button
                             className="navigation-button w-auto"
                             onClick={prevPage}
                             disabled={whichPage === 0}
@@ -138,9 +138,13 @@ const Home: React.FC = () => {
                                 cursor: whichPage > 0 ? 'pointer' : 'not-allowed',
                             }}
                         >
-                            &#8592;
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                                 className="bi bi-arrow-left-circle p-0" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                            </svg>
                         </button>
-                        <h2 className="recipes__title m-5 w-auto">{whichPage == 0 ? "Beliebteste Rezepte" : "Neuste Rezepte"}</h2>
+                        <h2 className="recipes__title m-5 w-50 text-center">{whichPage == 0 ? "Beliebteste Rezepte" : "Neuste Rezepte"}</h2>
                         <button
                             className="navigation-button w-auto h-auto"
                             onClick={nextPage}
@@ -152,28 +156,17 @@ const Home: React.FC = () => {
                                 cursor: 'pointer',
                             }}
                         >
-                            &#8594;
+                            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"
+                                 className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+                            </svg>
                         </button>
                     </div>
                     <div
                         className={`recipes__container ${animationClass}`}
                         style={{display: 'flex', alignItems: 'center'}}
-                    >
-                        <button
-                            className="navigation-button"
-                            onClick={prevPage}
-                            disabled={whichPage === 0}
-                            style={{
-                                background: 'none',
-                                border: 'none',
-                                fontSize: '2rem',
-                                cursor: whichPage > 0 ? 'pointer' : 'not-allowed',
-                            }}
-                        >
-                            &#8592;
-                        </button>
-
-                        {/* Recipes List */}
+                    >                        {/* Recipes List */}
                         <div className="recipes__list" style={{flex: 1, display: 'flex', overflowX: 'auto'}}>
                             {sampleRecipes!.map((recipe, index) => (
                                 <a
@@ -186,19 +179,6 @@ const Home: React.FC = () => {
                                 </a>
                             ))}
                         </div>
-
-                        <button
-                            className="navigation-button"
-                            onClick={nextPage}
-                            style={{
-                                background: 'none',
-                                border: 'none',
-                                fontSize: '2rem',
-                                cursor: 'pointer',
-                            }}
-                        >
-                            &#8594;
-                        </button>
                     </div>
                 </section>
             </main>
